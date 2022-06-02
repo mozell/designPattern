666ad96733dc083c9c0bef139dfee4b8d09590ad
mozell <mozellatto@gmail.com>
2022-06-02 20:36:35 +0900

    책임사슬 패턴(Chain of Responsibility Pattern)

---
+++ src/main/java/chainOfResponsibilityPattern/chainofresponsibility/LimitSupport.java
@@ -0,0 +1,15 @@
+package chainOfResponsibilityPattern.chainofresponsibility;
+
+public class LimitSupport extends Support {
+    private int limit;
+
+    public LimitSupport(String name, int limit) {
+        super(name);
+        this.limit = limit;
+    }
+
+    @Override
+    protected boolean resolve(Trouble trouble) {
+        return trouble.getNumber() < limit;
+    }
+}
