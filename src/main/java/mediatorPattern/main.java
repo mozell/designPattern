0ac22b2ef1d6a42111ed3a1d1cf99397e7000520
mozell <mozellatto@gmail.com>
2022-06-04 19:06:29 +0900

    중재자 패턴(Mediator Pattern)

---
+++ src/main/java/mediatorPattern/main.java
@@ -0,0 +1,9 @@
+package mediatorPattern;
+
+import mediatorPattern.mediator.LoginFrame;
+
+public class main {
+    public static void main(String[] args) {
+        new LoginFrame("Mediator sample");
+    }
+}
